3b1727ec1bf1bccbfba81ad752a4c3f1f13fddd3
Meshari93 <almeshari93@gmail.com>
2018-01-25 20:24:51 +0300

    v7

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
 
         /*
@@ -229,8 +230,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Image' => Intervention\Image\Facades\Image::class,
         'Form' => Collective\Html\FormFacade::class,
-
-
+        'Html' => Collective\Html\HtmlFacade::class,
 
 
 
